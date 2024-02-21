298a13899ce25c254dcdf9ff8106305cc3e41bcc
Aleks Ka <aleksandra.bm@mail.ru>
2024-02-21 20:30:04 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void main(String[] args) {
+        int ticket = 14700;
+        int minMoney = 20;
+        int mile = ticket / minMoney;
+
+        System.out.print("Начислено миль за покупку: " + mile);
+    }
 }
